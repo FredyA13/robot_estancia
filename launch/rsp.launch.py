523a6bd173b93a92c3cbb5b78c94ef0b4d2c6ce7
FredyA13 <fredy.alonzo2001@hotmail.com>
2023-03-23 05:13:47 -0600

    gaz_ros2_ctl

--- launch/rsp.launch.py
+++ launch/rsp.launch.py
@@ -21,7 +21,7 @@ def generate_launch_description():
     pkg_path = os.path.join(get_package_share_directory('robot_estancia'))
     xacro_file = os.path.join(pkg_path,'description','robot.urdf.xacro')
     # robot_description_config = xacro.process_file(xacro_file).toxml(),
-    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control])
+    robot_description_config = Command(['xacro ', xacro_file, ' use_ros2_control:=', use_ros2_control, ' sim_mode:=', use_sim_time])
 
     
     # Create a robot_state_publisher node
